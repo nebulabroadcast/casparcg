6a792e59b83fecb52a5ac9a32b6e8a5f3933bb8d
Martastain <martas@imm.cz>
2020-12-10 16:34:11 +0100

    Use caspar directory as a working dir

--- src/shell/run.sh
+++ src/shell/run.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $base_dir
+echo "Starting CasparCG server in $(pwd)"
 
 RET=5
-
 while [ $RET -eq 5 ]
 do
   LD_LIBRARY_PATH=lib bin/casparcg "$@"
   RET=$?
 done
-
